f9ef0c1d684f0a9042908f4a2878ee67c2f013e3
Austintjh19 <austintjh19@gmail.com>
2024-07-14 14:22:11 +0800

    Add Create Recent Event Request Handling

--- website_controller/website_api/serializers.py
+++ website_controller/website_api/serializers.py
@@ -1,9 +1,16 @@
 from rest_framework import serializers
 from .models import RecentEvent
 
-
+# For Get Request:
 class RecentEventSerializer(serializers.ModelSerializer):
     class Meta:
         model = RecentEvent
         fields = ('id', 'event_title', 'event_description', 'event_date',
                   'event_image', 'created_at')
+
+# For Post Request:       
+class CreateRecentEventSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = RecentEvent
+        fields = ('event_title', 'event_description', 'event_date',
+                        'event_image')
--- website_controller/website_api/urls.py
+++ website_controller/website_api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import RecentEventApiEndpoint
+from .views import RecentEventApiEndpoint, CreateRecentEventView
 
 urlpatterns = [
     path('recent-events', RecentEventApiEndpoint.as_view()),
+    path('create-recent-event/', CreateRecentEventView.as_view()),
 ]
